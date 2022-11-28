f47238cfdb92c52fcafdfd85e6885d90016d59ec
Arnaud Steiner <arnaudsteiner@orange.fr>
2022-11-28 16:02:27 +0100

    Initial commit with installation of NextJS - TypeScript - TailwindCSS

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Instagram Clone App</title>
+        <meta name="description" content="Instagram Clone App created for learning purposes" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
